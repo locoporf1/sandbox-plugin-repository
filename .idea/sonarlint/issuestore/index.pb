69e38e8ace61d2fea21f455e1e63fdfab617b14f
locoporf1 <locoporf1@gmail.com>
2021-01-26 13:00:25 +0100

    * Calling list I

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,13 @@
 
 j
 :out/production/sandbox-intellij-plugin/META-INF/plugin.xml,d\1\d143510d7f2ba5e8ac90d71f3fea577419b8d2c1
-W
-'src/org/plugin/sandbox/HelloAction.java,3\4\347ce802e06e05dcce22fd3dbfb497d8001e2962
 K
 sandbox-intellij-plugin.iml,8\a\8ad7e98252017d4dce3dde64fe2a5733656e4850
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+a
+1src/org/plugin/sandbox/actions/SandboxAction.java,3\0\30b7b6add460193821093e203d9dd33bfdff371d
+i
+9src/org/plugin/sandbox/services/MyApplicationService.java,3\e\3ef17771f43aa2cee91bc2401cb34dda351d879a
+a
+1src/org/plugin/sandbox/actions/MyActionGroup.java,b\0\b0b73877e0edfc2c8e872949c07a3d0515ea144d
--- src/org/plugin/sandbox/actions/MyActionGroup.java
+++ src/org/plugin/sandbox/actions/MyActionGroup.java
@@ -3,22 +3,34 @@ package org.plugin.sandbox.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class MyActionGroup extends DefaultActionGroup {
 
+  private static final String AMAZON_HOST = "sandboxserver-env.eba-tije36bi.us-east-1.elasticbeanstalk.com";
+
   @Override
   public void update(@NotNull AnActionEvent e) {
-    boolean writable = Files.isWritable(Paths.get("C:\\Users\\m.rodriguez.tena\\Desktop\\sandboxes.txt"));
-    e.getPresentation().setEnabled(writable);
+    HttpClient httpClient = new HttpClient();
+    httpClient.setHostConfiguration(getHostConfiguration());
+    HttpMethod httpMethod = getListMethod();
+    try {
+      int i = httpClient.executeMethod(httpMethod);
+      e.getPresentation().setEnabled(i == 200);
+      System.out.println("STATUS=" + i);
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
+    }
   }
 
   @NotNull
@@ -48,5 +60,15 @@ public class MyActionGroup extends DefaultActionGroup {
       e.getActionManager().registerAction(id, result);
     }
     return result;
+  }
+
+  private HostConfiguration getHostConfiguration() {
+    HostConfiguration result = HostConfiguration.ANY_HOST_CONFIGURATION;
+    result.setHost(AMAZON_HOST);
+    return result;
+  }
+  
+  private HttpMethod getListMethod() {
+    return new GetMethod("/list");
   }
 }
